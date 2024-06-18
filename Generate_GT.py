b8852b3c79d9ed3c342e6fab66dd8058a3d9f45e
moltapy <2263423153@qq.com>
2024-06-18 14:24:45 +0800

    Update Generate_GT.py

--- Generate_GT.py
+++ Generate_GT.py
@@ -8,8 +8,7 @@ gt_args = generate_gt_parser()
 # 读入并创建实例
 archaicvcf = ARCHAICVCF(gt_args.archaic)
 modernvcf = MODERNVCF(gt_args.modern)
-print(archaicvcf.get("archaic_name"))
-print(modernvcf.get("sample_index"))
+
 
 # 合并两个文件
 merge = archaicvcf+modernvcf
