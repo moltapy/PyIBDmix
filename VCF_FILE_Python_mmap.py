612dd4a7020f70f8a242542bd7d525f57d03fc5b
moltapy <2263423153@qq.com>
2024-06-20 13:17:13 +0800

    update mmap

---
+++ VCF_FILE_Python_mmap.py
@@ -0,0 +1,27 @@
+# 使用IO优化的策略彻底破产，考虑mmap或者生成器
+import gzip
+
+
+# 定义基类VCFFILE
+class VCFFILE:
+    def __init__(self,filename:str):
+
+        # 私有变量
+        self.__filename = filename
+        self.__header_array = _set_header(filename)
+        self.__genotype_array = _set_geno(filename)
+    
+def _set_header(filename):
+    headers = []  # 用于存储所有以 "##" 开头的行
+    with open(filename, "rt") as infile:  # 打开文件，自动处理缓冲
+        for line in infile:
+            if line.startswith("##"):
+                headers.append(line)  # 将注释行添加到列表中
+    return headers  # 返回收集的注释行
+
+
+        
+
+
+
+        
